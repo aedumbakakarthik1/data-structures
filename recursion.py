34c68a823abb4ae18b83cdb1813a4a2ac0d4d816
karthik <karthikaedumbaka1@gmail.com>
2022-08-30 18:14:45 +0530

    completed da.txt

---
+++ recursion.py
@@ -0,0 +1,31 @@
+"""
+recursion = a way od solving a promble by having a fuction calling itself
+
+->performing the same operation multiple times with different input parameters
+->in every step we try smaller input to mak the promblem samller
+->based on conditions in neeeded to stop the recursive otherwise infinite loop will occour
+
+code
+def openRecssianDoll(doll) :
+    if doll ==1:
+        print("all doll are opened")
+    else:
+        openRecssianDoll(doll-1)
+
+recursive thinking is really important in programming and it helps you break done big problems into smaller ones easier to use 
+
+
+when to choose the recursive function ?
+1)->if you can divide the problems into similar problems 
+->design an algorithm to compute the nth
+->implement a method to compute all
+->practice
+
+
+2)the prominent range of recursion in data structures like tree and graph
+3)interview
+4)it is used in many algorithm(divide and conquer,greedy and dynamic programming)
+
+
+
+"""
