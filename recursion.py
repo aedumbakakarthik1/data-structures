7d181afaffea18b6ab531bcd63b9d191c8589149
karthik <karthikaedumbaka1@gmail.com>
2022-09-01 18:33:35 +0530

    fixs the bugs

--- recursion.py
+++ recursion.py
@@ -67,4 +67,34 @@ recursionMethod(4)
 
 ### here the  recursionMethod was always us the stack memory(first in last out)
 
-"""when to use / avoid recursion ?"""
+"""when to use / avoid recursion ?
+when to use ?
+-> when we can easily breakdown a promblem into similar subproblem
+->when we can fine with extha overhaed (both time and space ) that comes with it
+-> when we need a quick working solution instead of efficiencyone
+-> when we use tree to
+-> when we use memoization in recursion
+
+when we need to avoid ?
+->if time and space complexity matters more
+->recursion users more memory . if we use embbeded memory .For example an application that takes more memory in the phone in not efficient
+->recurion ca be slow
+
+"""
+
+
+
+"""fibanacci number  - recursion
+fibanacci squence is a squence of numbers in which each number the sum of two preceding ones and the squence atart froms 0 and 1 and
+
+0,1,1,2,3,5,8,13,21,34,55,89....
+
+"""
+print("================================================")
+
+def fibanacci(n):
+    if n in [0,1]:
+        return n
+    else: 
+        return fibanacci(n-1)+fibanacci(n-2)
+print(fibanacci(6))
